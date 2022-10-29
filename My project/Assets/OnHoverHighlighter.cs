629e3df08865542d37a2c4cfdb3fcf8cb05d4767
grnatkimblel <55123133+grnatkimblel@users.noreply.github.com>
2022-10-29 18:20:43 -0400

    Made Opponent change color on mouse hover

---
+++ My project/Assets/OnHoverHighlighter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHoverHighlighter : MonoBehaviour
+{
+    Material intialMat;
+    //Material glowingMaterial;
+    RaycastHit hit;
+    int layerMask;
+    float maxDistance = 100f;
+    public LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        intialMat = GetComponent<Renderer>().material;
+        intialMat.color = Color.blue;
+        int layerMask = LayerMask.GetMask("Opponent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, maxDistance, mask.value))
+        {
+            intialMat.color = Color.green;
+        }
+        else
+        {
+            intialMat.color = Color.blue;
+        }
+
+        
+    }
+
+
+}
